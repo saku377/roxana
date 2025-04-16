6e8d11652540c4ad5595df4fce5ab5bb8fbbcaee
saku377 <roxanahellen2001@gmail.com>
2025-04-16 16:44:12 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,72 @@
+# # nombre="diego"
+# # edad = 64
+# # print("hola mundo" , nombre , edad)
+# print ("ingrese su nombre")
+# nombre = input()
+# print("ingrese su edad")
+# edad=input()
+# print("hola", nombre, "su edad es" , edad)
+# print("ingrese dos numeros")
+# n1=int(input())
+# n2=int(input())
+# print("la suma de los numeros es ", n1 + n2)
+
+
+# print("ingrese tres numeros")
+# n1=int(input())
+# n2=int(input())
+# n3=int(input())
+# prom=(n1 + n2 + n3) /3
+# print("el promedio de los numeros es " , prom)
+
+# if prom>=40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+
+
+# pedir edad al usuario y determinar si es mayor de edad 
+
+# print("ingrese su edad")
+# edad=int(input())
+# if edad >= 18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted no es mayor de edad")
+
+# mostrar segun criterio, menos de 12 años es niño
+# entre 12 y 17 es adolescente 
+# entre 18 y 64 es adulto
+# 65 y mas es adulto mayor
+
+# print("ingrese su edad")
+# edad=int(input())
+# if edad < 12:
+#     print("usted es un niño")
+# elif edad >= 12 and edad <18:
+#     print("usted es adolescente")
+# elif edad >=18 and edad <65:
+#     print ("usted es un adulto")
+# else:
+#     print("usted es un tatita")
+
+# ingresar 3 numeros y mostrar el mayor de los 3 
+
+print("ingrese tres numeros")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+
+if n1 > n2 and n1 > n3:
+    print("el numero mayor es " , n1)
+elif n2 > n1 and n2 > n3:
+    print("el numero mayor es " , n2)
+elif n3 > n1 and n3 > n2:
+    print("el numero mayor es " , n3)
+else:
+    print("error")
+
+
+
+    
+
