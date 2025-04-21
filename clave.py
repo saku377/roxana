1549c3afe19f24682eb06033984dbeb422464573
saku377 <roxanahellen2001@gmail.com>
2025-04-21 10:55:10 -0400

    roxana

---
+++ clave.py
@@ -0,0 +1,6 @@
+clave = 3344
+pas=int(input("ingrese su clave"))
+if pas == clave:
+    print("Bienvenido al sistema")
+else:
+    print("Clave invalida")
--- for.py
+++ for.py
@@ -27,39 +27,21 @@
 #         # suma += nota
 # prom =suma / cant 
 # print ("el promedio es " , prom)
+cantA=int(input("ingrese el numero de alumnos"))
 
-
-
-
-# cantA=int(input("ingrese el numero de alumnos"))
-
-# for j in range(cantA):
-#     print("ingrese la cantidad de notas" , j+1 ,"del alumno", j+1)
-#     cant = int(input())
-#     suma=0
-#     for i in range (cant):
-#             print("ingrese nota" , i+1 , "del alumno" , j+1)
-#             nota =int(input())
-#             suma =suma + nota 
-#             # suma += nota
-#     prom =suma / cant 
-#     print ("el promedio es " , prom)
-
-
-
+for j in range(cantA):
+    print("ingrese la cantidad de notas" , j+1 ,"del alumno", j+1)
+    cant = int(input())
+    suma=0
+    for i in range (cant):
+            print("ingrese nota" , i+1 , "del alumno" , j+1)
+            nota =int(input())
+            suma =suma + nota 
+            # suma += nota
+    prom =suma / cant 
+    print ("el promedio es " , prom)
     
 
 
 
-
-
-
-
-
-
-
-
-
-
-    
 
